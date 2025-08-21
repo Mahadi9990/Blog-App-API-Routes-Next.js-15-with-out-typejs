041992faac39f794f60b524d32daab83526a17f6
Mahadi9990 <mdshafayat9999@gmail.com>
2025-08-21 15:07:12 +0600

    solve layout.js file in body tag

--- blog-api-route/src/app/layout.js
+++ blog-api-route/src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body cz-shortcut-listen="true" data-new-gr-c-s-check-loaded="14.1249.0" data-gr-ext-installed="">
         {children}
       </body>
     </html>
